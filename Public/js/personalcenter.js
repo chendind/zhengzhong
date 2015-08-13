a76bd3c2cc1c5a56da91256beba3c83b97009b36
xsckev37 <x.kevin37@gmail.com>
2015-08-13 14:49:14 +0800

    add aboutus.html

--- Public/js/personalcenter.js
+++ Public/js/personalcenter.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".listItem:eq(0)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
-	$(".listItem:eq(1)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
+	$(".listItem:eq(1)").click(function(){location.href=rooturl+"/index.php/Home/Index/aboutus.html"});
 	$(".listItem:eq(2)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
 });
 
