7fee0e41f05e085f8fbc964bf3f6c1c15c9593a0
xsckev37 <x.kevin37@gmail.com>
2015-08-14 18:56:56 +0800

    add privatemail

--- Public/js/personalcenter.js
+++ Public/js/personalcenter.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(".listItem:eq(0)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
 	$(".listItem:eq(1)").click(function(){location.href=rooturl+"/index.php/Home/Index/aboutus.html"});
-	$(".listItem:eq(2)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
+	// $(".listItem:eq(2)").click(function(){location.href=rooturl+"/index.php/Home/Order/orderlist.html"});
+	$(".title_img_right").click(function(){location.href="privatemail.html";});
 });
 
