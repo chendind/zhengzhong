fa3cc5f7d692c920b31469e09f9ec418be7d6f59
xsckev37 <x.kevin37@gmail.com>
2015-08-18 10:22:29 +0800

    main.html ver 0.2

--- Public/js/homepage.js
+++ Public/js/homepage.js
@@ -2,9 +2,9 @@ $(document).ready(function(e) {
 	var _width = document.body.offsetWidth;
 	var _height = document.body.offsetHeight;
 	$(".news_box").css({"width":_width,"height":_height*0.28});	
-	var exp_box_height = _height - $(".ui-header").height() - $(".ui-footer").height() - $(".news_box").height() - $(".news_box2").height() - 5;
-	$(".exp_box").css({"height":exp_box_height});	
-	$(".exp_box li").css({"width":"100%","height":exp_box_height*0.32});
+	// var exp_box_height = _height - $(".ui-header").height() - $(".ui-footer").height() - $(".news_box").height() - $(".news_box2").height() - 5;
+	// $(".exp_box").css({"height":exp_box_height});	
+	// $(".exp_box li").css({"width":"100%","height":exp_box_height*0.32});
 	$(".news_box img").each(function(index, element) {
         $(this).css({"width":_width,"height":_height*0.276});
     });
@@ -27,5 +27,5 @@ $(document).ready(function(e) {
 			width:$('#scroll_position').width()
 		});
 	});
-	console.log($(".news_box2").width());
+	//console.log($(".news_box2").width());
 });
